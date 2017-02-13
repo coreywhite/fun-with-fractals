ed007dac5c77300da2bd2ec2dbd091001e1e2a88
Corey White <white.corey.a@gmail.com>
2017-02-12 20:38:51 -0500

    Added click to zoom and recenter

--- src/index.ts
+++ src/index.ts
@@ -16,17 +16,17 @@ class Color {
 class MandelbrotRenderer {
     private _display: CanvasDisplay;
     private _hasDisplay: boolean;
-    private readonly fractalWidth: number; 
-    private readonly fractalHeight: number;
-    private readonly centerX: number;
-    private readonly centerY: number;
+    private fractalWidth: number; 
+    private fractalHeight: number;
+    private centerX: number;
+    private centerY: number;
     private readonly maxIterations: number;
     private readonly xResolution: number;
     private readonly yResolution: number;
     private readonly fractalData: number[][];
     private readonly imgData: ImageData;
 
-    constructor(readonly canvas: HTMLCanvasElement) {
+    constructor(readonly canvas: HTMLCanvasElement, readonly coordinatesDisplay: HTMLElement) {
         
         //The Mandelbrot set is contained in the closed disk of radius 2 around the origin, so the
         //initial width and height are both 4 and the initial center is at (0, 0) in the complex plane.
@@ -78,11 +78,19 @@ class MandelbrotRenderer {
             }
         }
     }
+
+    private getFractalCoordinatesAtCanvasCoordinates(canvasCoordinates:[number, number]): [number, number] {
+        var x = this.centerX - (this.fractalWidth / 2) + canvasCoordinates[0] * this.fractalWidth / this.canvas.width;
+        var y = this.centerY + (this.fractalHeight / 2) - canvasCoordinates[1] * this.fractalHeight / this.canvas.height;
+        return [x, y];
+    }
+
     private getFractalValueAtImagePosition(imageRow: number, imageCol: number): number  {
         var fractalRow = Math.floor(imageRow * this.yResolution / this.imgData.height);
         var fractalCol = Math.floor(imageCol * this.xResolution / this.imgData.width);
         return this.fractalData[fractalRow][fractalCol];
     }
+
     private setPixel(row: number, col: number, color: Color): void {
         //Image Data is stored in a one-dimensional array as 4 separate values: RGBA
         this.imgData.data[4 * (row * this.imgData.width + col) + 0] = color.R;    //Red
@@ -90,6 +98,7 @@ class MandelbrotRenderer {
         this.imgData.data[4 * (row * this.imgData.width + col) + 2] = color.B;        //Blue
         this.imgData.data[4 * (row * this.imgData.width + col) + 3] = color.A;      //Alpha
     }
+
     private mapColor(fractalValue: number): Color {
         if(fractalValue == this.maxIterations) {
             return new Color(0, 0, 0);
@@ -97,6 +106,7 @@ class MandelbrotRenderer {
             return new Color(Math.min(255, (fractalValue - 1) * 20), 0, 0);
         }
     }
+
     render(): void {
         if(!this._hasDisplay) {
             return;
@@ -110,8 +120,40 @@ class MandelbrotRenderer {
         }
         this._display.render(this.imgData);
     }
+
+    recenter(newCenter: [number, number], fractalWidth?: number, fractalHeight?: number): void {
+        this.centerX = newCenter[0];
+        this.centerY = newCenter[1];
+        this.fractalWidth = fractalWidth || this.fractalWidth;
+        this.fractalHeight = fractalHeight || this.fractalHeight;
+        
+        this.render();
+    }
+
+    displayCoordinates(coordinates: [number, number]): void {
+        this.coordinatesDisplay.innerHTML = `(${coordinates[0].toFixed(4)},${coordinates[1].toFixed(4)})`;
+    }
+
+    handleMouseOver = (event: MouseEvent) => {
+        var rect = this.canvas.getBoundingClientRect();
+        var canvasCoordinates: [number, number] = [event.clientX - rect.left, event.clientY - rect.top];
+        var fractalCoordinates = this.getFractalCoordinatesAtCanvasCoordinates(canvasCoordinates);
+        
+        this.displayCoordinates(fractalCoordinates);        
+    }
+
+    handleClick = (event: MouseEvent) => {
+        var rect = this.canvas.getBoundingClientRect();
+        var canvasCoordinates: [number, number] = [event.clientX - rect.left, event.clientY - rect.top];
+        var fractalCoordinates = this.getFractalCoordinatesAtCanvasCoordinates(canvasCoordinates);
+
+        this.recenter(fractalCoordinates, this.fractalWidth / 2, this.fractalHeight / 2);
+    }
 }
 
 var canvas = <HTMLCanvasElement> document.getElementById('fractal-canvas');
-var mandelbrot = new MandelbrotRenderer(canvas);
+var coordinates = <HTMLElement> document.getElementById("coordinates");
+var mandelbrot = new MandelbrotRenderer(canvas, coordinates);
 mandelbrot.render();
+canvas.addEventListener("click", mandelbrot.handleClick);
+canvas.addEventListener("mousemove", mandelbrot.handleMouseOver);
