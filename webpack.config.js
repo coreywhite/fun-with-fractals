f6f0ef96466970d9341393b71464f9f395d342da
Corey White <white.corey.a@gmail.com>
2017-05-23 20:46:37 -0400

    Re-arranged file structure to work better with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ module.exports = {
     entry: "./src/index.tsx",
     output: {
         filename: "bundle.js",
-        path: __dirname + "/dist"
+        publicPath: "/build/",
+        path: __dirname + "/dist/build"
     },
 
     // Enable sourcemaps for debugging webpack's output.
@@ -27,8 +28,8 @@ module.exports = {
     // assume a corresponding global variable exists and use that instead.
     // This is important because it allows us to avoid bundling all of our
     // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    }
+    // externals: {
+    //     "react": "React",
+    //     "react-dom": "ReactDOM"
+    // }
 };
